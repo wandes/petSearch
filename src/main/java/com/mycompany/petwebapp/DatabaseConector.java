865b280e07757dc4e2ec7ae031f9623c0a156a46
wandes <42286301+wandes@users.noreply.github.com>
2019-03-26 22:45:04 -0300

    testando conexao com o banco

--- src/main/java/com/mycompany/petwebapp/DatabaseConector.java
+++ src/main/java/com/mycompany/petwebapp/DatabaseConector.java
@@ -21,15 +21,19 @@ public class DatabaseConector {
     
     private static final String DRIVER = "org.postgresql.Driver";
     
-    private static final String URL = "postgres://wtdfwklzzaoenv:a3c7fd4329d7e123ba36da9e6593aa2f7933344c4866cc24e5b7770725ea1f66@ec2-50-19-109-120.compute-1.amazonaws.com:5432/df1vprb22k6md4";
+    private static final String URL = "jdbc:postgresql://ec2-50-19-109-120.compute-1.amazonaws.com:5432/df1vprb22k6md4?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
     
     private static final String USER = "wtdfwklzzaoenv";
     
-    private static final String PASS = "a3c7fd4329d7e123ba36da9e6593aa2f7933344c4866cc24e5b7770725ea1f66"; 
+    private static final String PASS = "a3c7fd4329d7e123ba36da9e6593aa2f7933344c4866cc24e5b7770725ea1f66";
+    
     
     public static String conection() throws SQLException{
-     
+    
+      
+    
      try{   
+           
      Class.forName(DRIVER);
      String status = null;
      Connection con = DriverManager.getConnection(URL, USER, PASS);       
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -18,7 +18,7 @@
 	<div align="center">
                     <h1>Pet Search</h1>
                     <h2 style="color: red;" align="center">EM DESENVOLVIMENTO!!</h2>	
-                    <h3><%=DatabaseConector.conection()%></h3>
+                    <h3><%= (DatabaseConector.conection())%></h3>
                     <img src="https://mercado.etc.br/wp-content/uploads/2018/04/sinais-que-nos-indicam-se-um-cachorro-e-feliz.jpg">
 	</div>
     </body>
