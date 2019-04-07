4a829d1bba6d66093b87bc057a0d63074028a8cd
wandes <42286301+wandes@users.noreply.github.com>
2019-04-07 20:50:03 -0300

    servlet acessando objetos e fazendo Insert e read funcionar

--- src/main/java/br/com/petsearch/mvc/controller/Servletcontroller.java
+++ src/main/java/br/com/petsearch/mvc/controller/Servletcontroller.java
@@ -5,8 +5,12 @@
  */
 package br.com.petsearch.mvc.controller;
 
+import br.com.petsearch.mvc.dao.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,32 +20,25 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Wandes
  */
-public class ControllerServlet extends HttpServlet {
+public class Servletcontroller extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet controllerServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet controllerServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        if (request.getParameter("add-user") != null) {
+            try {
+                User.insertUser(request.getParameter("name"), request.getParameter("email"), request.getParameter("password"),
+                        Integer.parseInt(request.getParameter("telephone")), request.getParameter("gender"));
+                RequestDispatcher rd = request.getRequestDispatcher("/add-user.jsp");
+                rd.forward(request, response);
+            } catch (Exception ex) {
+                Logger.getLogger(Servletcontroller.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
